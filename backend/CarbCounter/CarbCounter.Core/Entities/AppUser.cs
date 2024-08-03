1657944b058c429f8f805bd7318061758b9c460a
CallumS <dev@callums005.net>
2024-08-03 14:22:14 +0100

    Normalized string values with registration and login details

--- backend/CarbCounter/CarbCounter.Core/Entities/AppUser.cs
+++ backend/CarbCounter/CarbCounter.Core/Entities/AppUser.cs
@@ -7,9 +7,14 @@ namespace CarbCounter.Core.Entities;
 public class AppUser : IdentityUser, ITimeStamped
 {
     public required string Forename { get; set; } = null!;
+    public string NormalizedForename => Forename.ToLower();
     public required string Surname { get; set; } = null!;
+    public string NormalizedSurname => Surname.ToLower();
     public required DateTime CreatedAt { get; init; }
     public DateTime? UpdatedAt { get; set; }
     public required EnUserType UserType { get; set; } = EnUserType.User;
     public int UserTypeId => (int)UserType;
+    public required EnRegistrationStatus RegistrationStatus { get; set; } = EnRegistrationStatus.AccountCreated;
+    public int RegistrationStatusId => (int)RegistrationStatus;
+    public DateTime? LastLogon { get; set; } = null;
 }
