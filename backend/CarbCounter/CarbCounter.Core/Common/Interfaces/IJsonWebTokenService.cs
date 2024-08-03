858d00352ffbd381307f4b2c2458c592523cd475
CallumS <dev@callums005.net>
2024-08-03 14:22:48 +0100

    JWT service

---
+++ backend/CarbCounter/CarbCounter.Core/Common/Interfaces/IJsonWebTokenService.cs
@@ -0,0 +1,9 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace CarbCounter.Core.Common.Interfaces;
+
+public interface IJsonWebTokenService
+{
+    public JwtSecurityToken GetToken(List<Claim> authClaims);
+}
---
+++ backend/CarbCounter/CarbCounter.Core/Services/JsonWebTokenService.cs
@@ -0,0 +1,33 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using CarbCounter.Core.Common.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace CarbCounter.Core.Services;
+
+public class JsonWebTokenService : IJsonWebTokenService
+{
+    private readonly IConfiguration _configuration;
+
+    public JsonWebTokenService(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public JwtSecurityToken GetToken(List<Claim> authClaims)
+    {
+        SymmetricSecurityKey authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Secret"]));
+
+        JwtSecurityToken token = new JwtSecurityToken(
+            issuer: _configuration["JWT:ValidIssuer"],
+            audience: _configuration["JWT:ValidAudience"],
+            expires: DateTime.Now.AddHours(int.Parse(_configuration["JWT:Duration"])),
+            claims: authClaims,
+            signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
+        );
+
+        return token; 
+    }
+}
